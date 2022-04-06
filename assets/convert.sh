20388fc5abc1075131ec207b1f7b3b8b23e5cef2
Patrick Bucher <patrick.bucher@seantis.ch>
2022-04-06 10:41:44 +0200

    converted corner images from SVG to PNG

--- assets/convert.sh
+++ assets/convert.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/bash
 
-for gif in *.gif
-do
-    png=$(echo $gif | sed 's/\.gif$/.png/')
-    convert $gif $png
-done
+function gif_to_png() {
+    for gif in *.gif
+    do
+        png=$(echo $gif | sed 's/\.gif$/.png/')
+        convert $gif $png
+    done
+}
 
-for svg in *.svg
-do
-    png=$(echo $gif | sed 's/\.svg$/.png/')
-    convert $svg -resize 250x -transparent white $png
-done
+function svg_to_png() {
+    for svg in *.svg
+    do
+        png=$(echo $svg | sed 's/\.svg$/.png/')
+        convert $svg -transparent white $png
+    done
+}
+
+if [ "$1" = "gif" ]
+then
+    gif_to_png
+elif [ "$1" = "svg" ]
+then
+    svg_to_png
+fi
 
